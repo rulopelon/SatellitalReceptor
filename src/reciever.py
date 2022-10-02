7cace80483349fa38df4c7f1d3083ec02d014e6f
rulopelon <201801914@alu.comillas.edu>
2022-10-02 13:20:58 +0200

    load variables from file implemented

--- src/reciever.py
+++ src/reciever.py
@@ -4,17 +4,21 @@ import matplotlib.pyplot as plt
 from algorithms import *
 from qpskReciever import *
 from libhackrf import *
-
+import json
 
 #Variable declaration
-fs = 1e6 # Hz
-center_freq = 100e6 # Hz
-num_samples = 10000 # number of samples returned per call to rx()
+with open('src/parameters.json', 'r') as f:
+  variables = json.load(f)
+
+#Variable declaration
+fs = int(variables['fs']) # Hz
+center_freq = int(variables['center_freq']) # Hz
+num_samples = int(variables['num_samples_rx']) # number of samples returned per call to rx()
+alpha = float(variables['alpha'])
+symbol_period = int(variables['symbol_period'])
 recieve = True
-alpha = 0.6
-symbol_period = 4
-length_rrcos_filter  = 100
-search_multiplier = 0.75
+
+
 
 hackrf = HackRF()
 
@@ -32,33 +36,12 @@ sdr.rx_rf_bandwidth = int(fs) # filter width, just set it to the same as sample 
 sdr.rx_buffer_size = num_samples
 """
 
-
-#Filter to filter the recieved signal
-RRCosFilter = getFilter(length_rrcos_filter,alpha,symbol_period,fs)
-
-# The filter is shown
-showSpectrum(RRCosFilter,fs)
-
 i = 0
 while recieve:
     samples = hackrf.read_samples(num_samples) # receive samples
 
     # Save the samples
-    np.save(str(i)+'.npy',samples)
+    np.save(str(i)+'.npy',samples)    
 
-    print("Recieved signal")
-    showSpectrum(samples,fs)
-    
-    # Recovering frecuency and phase 
-    synchronized_signal = costasAlgo(samples,fs)
-
-    # Filter the samples
-    filtered_signal = filterSignal(synchronized_signal,RRCosFilter)
-
-    print("Filtered signal")
-    showSpectrum(filtered_signal,fs)
-
-    #Decoding the bit on each channel
-    bit_vector = demodulator(filtered_signal,symbol_period,fs,search_multiplier)
-    # Saving the decoded signal
+    # Add one iteration
     i =i+1    
